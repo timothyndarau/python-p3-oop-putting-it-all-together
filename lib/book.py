6e95233ce83a865fd3fb4eab9f44d317cce5c799
timothy <timothy.ndarau@student.moringaschool.com>
2023-08-22 22:02:22 +0300

    done

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,22 @@
 #!/usr/bin/env python3
 
 class Book:
-    pass
+    def __init__(self, title, page_count):
+        self.title = title
+        self._page_count = page_count  # Store page_count as a private attribute with an underscore
+
+    @property
+    def page_count(self):
+        return self._page_count
+
+    @page_count.setter
+    def page_count(self, new_page_count):
+        if isinstance(new_page_count, int):
+            self._page_count = new_page_count
+        else:
+            print("page_count must be an integer")
+
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
     
         
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,24 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    def __init__(self, brand, size):
+        self.brand = brand
+        self._size = size  # Store size as a private attribute with an underscore
+        self.condition = "New"
+
+    @property
+    def size(self):
+        return self._size
+
+    @size.setter
+    def size(self, new_size):
+        if isinstance(new_size, int):
+            self._size = new_size
+        else:
+            print("size must be an integer")
+
+    def cobble(self):
+        print("Your shoe is as good as new!")
+        self.condition = "New"
+
+
